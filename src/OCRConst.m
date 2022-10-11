a6c01c8b80a53247bc9bc5a61e53dc7b9549b9b1
LAPTOP-HTIFHJ5U\crist <smartlord7@gmail.com>
2022-10-11 15:21:46 +0100

    MASTER: Add functions to build ass. mem. and perceptron filter.

---
+++ src/OCRConst.m
@@ -0,0 +1,16 @@
+classdef OCRConst
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties (Constant)
+        N_FILTER_INPUTS = 256;
+        N_FILTER_LAYERS = 1;
+        N_FILTER_OUTPUTS = 256;
+        FILTER_WEIGHT_INIT_MIN = 0;
+        FILTER_WEIGHT_INIT_MAX = 1;
+        ASSOCIATIVE_FILTER_TRAIN_FUNCTION = 'traingda';
+        PERCEPTRON_FILTER_TRAIN_FUNCTION = 'trainc';
+        PERCEPTRON_FILTER_ADAPT_FUNCTION = 'learnp';
+        N_CLASSIFIER_INPUTS = 256;
+    end
+end
